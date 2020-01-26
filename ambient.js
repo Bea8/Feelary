7981e142f0c9585ccd75a590c51fc0c20df6de97
Bea <btmaj8@gmail.com>
2020-01-26 14:47:04 +0100

    Stylesheet update

--- ambient.js
+++ ambient.js
@@ -8,21 +8,27 @@ $(document).ready(function(){
       $(this).toggleClass("rotate");
       $("body").toggleClass("newCol_1");
       $("footer").toggleClass("newCol_1f");
-      $("#sunset, #hart, #cloud").removeClass("total_eclipse").toggleClass("outline");
+      $("#sunset, #hart, #cloud").toggleClass("total_eclipse").toggleClass("outline");
 
   });
 
   $("#sunset").click(function(){
+      $(this).toggleClass("total_eclipse");
       $("g.tides").toggleClass("flow");
+      $("#sun, #sunset").removeClass("outline");
   });
 
   $("#hart").click(function(){
+      $(this).toggleClass("total_eclipse");
       $(this).toggleClass("pulse");
+      $("#sun, #sunset, #hart").removeClass("outline");
   });
 
   $("#cloud").click(function(){
+      $(this).toggleClass("total_eclipse");
       $("#bolt2").toggleClass("lightning2");
       $("#bolt1").toggleClass("lightning1");
+      $("#sunset, #hart, #sun, #cloud").removeClass("outline");
 
   });
 
